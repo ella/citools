895c49d5badd17e4558d0ace2be22609234f4f6c
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-04-23 21:23:48 +0200

    Initial directory structure (+ few intro words)

---
+++ doc/index.rst
@@ -0,0 +1,40 @@
+====================
+CI tools
+====================
+
+Ultimate goal of Continuous Integration tools is to provide "integration button", magic key hit that will do everything needed for product to build & deploy. While there are some tools available, none of those fullfills our real-world needs.
+
+Basic idea is simple: use setup.py in similar fashion as Makefiles or ant, but take advantage of setuptools plugin system to provide globally available commands, easily configurable for every project.
+
+We are making some assumptions about project structure and needs. If you need to configure something more, let us know, or just `fork us at github <http://github.com/ella/citools/tree/master>`_ and send us Your pull request.
+
+Licensed under `BSD <http://www.opensource.org/licenses/bsd-license.php>`_, this library is maintained by Ella team from Centrum Holdings. For feedback, ideas, bug reports and friends, let us know in `mailing list <http://groups.google.com/group/ella-project>`_.
+
+.. toctree::
+   :maxdepth: 2
+
+----------------------------
+On (continuous) versioning
+----------------------------
+
+----------------------------
+(Django) web environment
+----------------------------
+
+----------------------------
+Build process
+----------------------------
+
+----------------------------
+Testing
+----------------------------
+
+----------------------------
+Working with databases
+----------------------------
+
+----------------------------
+Distribution and Deployment
+----------------------------
+
+
---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+__versionstr__ = '0.0.1.0'
+setup(
+    name = 'citools'
+    version = __versionstr__,
+    description = 'Coolection of plugins to help with building CI system',
+    long_description = '\n'.join((
+        'CI Tools',
+        '',
+        'Ultimate goal of CI system is to provide single "integration button"',
+	'to automagically do everything needed for creating a release',
+	"(and ensure it's properly build version).",
+	"This package provides a set of setuptools plugins (setup.py commands)",
+	"to provide required functionality and make CI a breeze.",
+	"Main aim of this project are Django-based applications, but it's usable",
+	"for generic python projects as well.",
+    )),
+    author = 'centrum holdings s.r.o',
+    author_email='devel@centrumholdings.com',
+    license = 'BSD',
+    url='http://github.com/ella/citools/tree/master',
+
+    packages = find_packages(
+        where = '.',
+        exclude = ('docs', 'tests')
+    ),
+
+    include_package_data = True,
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+#    entry_points = {
+#        'setuptools.file_finders': ['dummy = setuptools_entry:dummylsfiles'],
+#    },
+    install_requires = [
+        'setuptools>=0.6b1',
+    ],
+#    setup_requires = [
+#        'setuptools_git',
+#    ],
+)
+
