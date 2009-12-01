15628e171698eddc0d61b6c8127b39ec76430d83
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-12-01 19:20:16 +0100

    Hide another pymongo import

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,14 +1,18 @@
 from nose.plugins.skip import SkipTest
-from pymongo.errors import ConnectionFailure
 
 import os
 from unittest import TestCase
 
-from citools.mongo import get_mongo_and_database_connections
 
 
 class MongoTestCase(TestCase):
     def setUp(self):
+        try:
+            from pymongo.errors import ConnectionFailure
+            from citools.mongo import get_mongo_and_database_connections
+        except ImportError, e:
+            import traceback as t
+            raise SkipTest("Error when importing dependencies (pymongo not installed?): %s" % t.format_exception())
         try:
             self.database, self.connection = get_mongo_and_database_connections(
                 hostname=os.environ.get("MONGODB_HOSTNAME", "localhost"),
