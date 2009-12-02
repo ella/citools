d80958f1585cbf956bdff23b087320217ae17555
Lukas Linhart <lukas.linhart@centrumholdings.com>
2009-12-02 14:37:34 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = (0, 1, 0)
+VERSION = (0, 2, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
