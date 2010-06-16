836fe50517e6363d0dabfcb213eb34943564ac9b
Lukas Linhart <lukas.linhart@centrumholdings.com>
2010-06-16 14:29:04 +0200

    Upgrade not needed for install (editable environments updates automagically)

--- citools/paver.py
+++ citools/paver.py
@@ -42,7 +42,7 @@ def integrate(args):
 
 @task
 def install_dependencies():
-    sh('pip install --upgrade -r requirements.txt')
+    sh('pip install -r requirements.txt')
 
 @task
 def bootstrap():
